e2114a6f1980fa27c9b2e215ed0b2231b82e48dd
Sarthak <sarthaksingh0608@gmail.com>
2021-05-18 17:42:26 +0530

    added and changed all intial files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Let's Go Baby</h1>
+
+      
+    </div>
+  );
+}
+
+export default App;
